c1f754ae7cbd51bf1c42c2a4a63d969704b0d0a1
Thiago Menezes <113785728+Thiago-Develooper@users.noreply.github.com>
2024-09-26 17:33:30 -0300

    Adicionado APIManager que é genérico

--- Macro02/Macro02/Model/LiturgiaDiariaAPI.swift
+++ Macro02/Macro02/Model/LiturgiaDiariaAPI.swift
@@ -98,3 +98,64 @@ class LiturgiaDiariaAPI {
         task.resume()
     }
 }
+
+import Foundation
+
+// Manager genérico para fazer chamadas de API e decodificar qualquer tipo de objeto Decodable
+class APIManager {
+    
+    // Função genérica que faz uma requisição e decodifica a resposta
+    func fetchData<T: Decodable>(from urlString: String, responseType: T.Type, completion: @escaping (Result<T, Error>) -> Void) {
+        // Valida a URL
+        guard let url = URL(string: urlString) else {
+            print("URL inválida")
+            return
+        }
+        
+        // Cria a tarefa de requisição
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                print("Erro ao fazer a requisição: \(error.localizedDescription)")
+                completion(.failure(error))
+                return
+            }
+            
+            guard let data = data else {
+                print("Dados não recebidos")
+                return
+            }
+
+            // Verificar o código de resposta HTTP
+            if let httpResponse = response as? HTTPURLResponse {
+                if httpResponse.statusCode != 200 {
+                    print("Erro na resposta da API: \(httpResponse.statusCode)")
+                    return
+                }
+            }
+
+            // Tenta converter os dados recebidos para JSON e imprimir
+            do {
+                if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
+                    print("Resposta JSON da API: \(json)")
+                } else {
+                    print("A resposta não é um dicionário JSON.")
+                }
+            } catch {
+                print("Erro ao converter para JSON: \(error.localizedDescription)")
+            }
+
+            // Decodifica os dados para o tipo genérico T
+            do {
+                let decoder = JSONDecoder()
+                let decodedData = try decoder.decode(T.self, from: data)
+                completion(.success(decodedData))
+            } catch {
+                print("Erro ao decodificar dados: \(error)")
+                completion(.failure(error))
+            }
+        }
+        
+        task.resume()
+    }
+}
+
