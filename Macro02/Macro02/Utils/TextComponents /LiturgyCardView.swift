e01a0bfcd2d0e809aba56ab875c46275f2b1ce22
Thiago Menezes <113785728+Thiago-Develooper@users.noreply.github.com>
2024-10-03 17:01:47 -0300

    refactor: Adicionadas constraints e ajustes no LiturgyCardView
    - Corrigida documentação no topo da classe
    - Adicionada propriedade `monthNameLabel` e ajustado o método `update(with:)`
    - Implementadas constraints para os componentes weekNumberLabel, dayNameLabel, dayNumberLabel, monthNameLabel, e yearNumberLabel
    - Refatoração para garantir que os elementos estão corretamente posicionados na view

--- Macro02/Macro02/Utils/TextComponents /LiturgyCardView.swift
+++ Macro02/Macro02/Utils/TextComponents /LiturgyCardView.swift
@@ -12,14 +12,15 @@ import UIKit
 /// O card contém as seguintes informações:
 /// - Semana atual da liturgia exibida no `weekNumberLabel`.
 /// - Dia da semana atual em formato de texto no `dayNameLabel`.
-/// - Dia da liturggia em formado de número `dayNameLabel`
-/// - Dia da liturgia em formato de número `yearNumberLabel`
+/// - Dia da liturgia em formato de número no `dayNumberLabel`.
 /// - Mês em formato de texto no `monthNameLabel`.
 /// - Ano no formato de número em `yearNumberLabel`.
 ///
 /// Este componente é utilizado no protótipo de média na View de liturgia para exibir dados relacionados à data.
 
 class LiturgyCardView: UIView {
+    
+    private var dataLiturgia = String()
         
     private let weekNumberLabel = TextComponent()
     private let dayNameLabel = TextComponent()
@@ -47,17 +48,47 @@ class LiturgyCardView: UIView {
         addSubview(weekNumberLabel)
         addSubview(dayNameLabel)
         addSubview(dayNumberLabel)
+        addSubview(monthNameLabel)
         addSubview(yearNumberLabel)
-    }
-    
-    private func constrains() {
         
+        setConstraints()
     }
     
     func update(with liturgia: Liturgia) {
         weekNumberLabel.text = liturgia.data ?? "no data week label"
         dayNameLabel.text = liturgia.dia
         dayNumberLabel.text = liturgia.data ?? "no dayNumberLabel"
+        monthNameLabel.text = liturgia.data ?? "no monthNameLabel"
         yearNumberLabel.text = "\(liturgia.data) / \(liturgia.data)"
+        self.dataLiturgia = liturgia.data ?? "no data"
+        print(self.dataLiturgia)
+    }
+}
+
+extension LiturgyCardView {
+    private func setConstraints() {
+        weekNumberLabel.translatesAutoresizingMaskIntoConstraints = false
+        dayNameLabel.translatesAutoresizingMaskIntoConstraints = false
+        dayNumberLabel.translatesAutoresizingMaskIntoConstraints = false
+        monthNameLabel.translatesAutoresizingMaskIntoConstraints = false
+        yearNumberLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            weekNumberLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 16),
+            weekNumberLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 16),
+            
+            dayNameLabel.topAnchor.constraint(equalTo: weekNumberLabel.bottomAnchor, constant: 8),
+            dayNameLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 16),
+            
+            dayNumberLabel.topAnchor.constraint(equalTo: dayNameLabel.bottomAnchor, constant: 8),
+            dayNumberLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 16),
+            
+            monthNameLabel.topAnchor.constraint(equalTo: dayNumberLabel.bottomAnchor, constant: 8),
+            monthNameLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 16),
+            
+            yearNumberLabel.topAnchor.constraint(equalTo: monthNameLabel.bottomAnchor, constant: 8),
+            yearNumberLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 16),
+            yearNumberLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -16)
+        ])
     }
 }
