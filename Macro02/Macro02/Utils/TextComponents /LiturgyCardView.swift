bcb1f2da7579069b170e4ea8c6eeb10e9ca7f6f8
Thiago Menezes <113785728+Thiago-Develooper@users.noreply.github.com>
2024-10-03 15:06:55 -0300

    docs: Adicionada documentação à classe LiturgyCardView
    - Incluída descrição detalhada no topo da classe LiturgyCardView
    - Documentação descreve as informações exibidas no card de liturgia
    - Explicação de cada UILabel e sua função no contexto da liturgia

--- Macro02/Macro02/Utils/TextComponents /LiturgyCardView.swift
+++ Macro02/Macro02/Utils/TextComponents /LiturgyCardView.swift
@@ -7,6 +7,16 @@
 
 import UIKit
 
+/// Card que exibe informações da liturgia na `LiturgyView`.
+///
+/// O card contém as seguintes informações:
+/// - Semana atual da liturgia exibida no `weekLabel`.
+/// - Dia da semana atual em formato de texto no `dayNameLabel`.
+/// - Dia do mês em formato numérico no `dayNumberLabel`.
+/// - Mês abreviado e ano completo no `monthYearLabel`.
+///
+/// Este componente é utilizado no protótipo de média na View de liturgia para exibir dados relacionados à data.
+
 class LiturgyCardView: UIView {
     
     private let weekLabel = UILabel()
