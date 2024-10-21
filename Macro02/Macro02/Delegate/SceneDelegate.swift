10284d75b66b2b17b7b8ac547087265f182c23ec
Gabriel Ribeiro Noronha <gabrielrib98@gmail.com>
2024-10-21 11:24:35 -0300

    light mode

--- Macro02/Macro02/Delegate/SceneDelegate.swift
+++ Macro02/Macro02/Delegate/SceneDelegate.swift
@@ -16,6 +16,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         if let windowScene = scene as? UIWindowScene {
             let window = UIWindow(windowScene: windowScene)
             
+            // Apply Dark Mode Override Globally
+            window.overrideUserInterfaceStyle = .light
+            
             //MARK: Fluxo normal
             let applicationCoordinator = ApplicationCoordinator(window: window)
             
