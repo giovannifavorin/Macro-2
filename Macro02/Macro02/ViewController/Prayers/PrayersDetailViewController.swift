907588d102c5da96eba9845fcce22ce142cf6023
Thiago Menezes <113785728+Thiago-Develooper@users.noreply.github.com>
2024-10-21 17:56:02 -0300

    PrayerCell: UITableViewCell  criada

--- Macro02/Macro02/ViewController/Prayers/PrayersDetailViewController.swift
+++ Macro02/Macro02/ViewController/Prayers/PrayersDetailViewController.swift
@@ -39,9 +39,10 @@ class PrayersDetailViewController: UIViewController, UITableViewDelegate, UITabl
         tableView = UITableView(frame: view.bounds)
         tableView.delegate = self
         tableView.dataSource = self
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "PrayersCell")
-        view.addSubview(tableView)
         
+        // Registrar a célula personalizada
+        tableView.register(PrayerCell.self, forCellReuseIdentifier: "PrayerCell")
+        view.addSubview(tableView)
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -49,11 +50,11 @@ class PrayersDetailViewController: UIViewController, UITableViewDelegate, UITabl
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "PrayersCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PrayerCell", for: indexPath) as! PrayerCell
         
         let prayer = prayers[indexPath.row]
         
-        // Configura o texto e a imagem da célula
+        // Configura o texto da célula
         cell.textLabel?.text = prayer.title
         
         return cell
@@ -68,5 +69,30 @@ class PrayersDetailViewController: UIViewController, UITableViewDelegate, UITabl
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 70
     }
+}
 
+class PrayerCell: UITableViewCell {
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        // Configuração da célula
+        self.contentView.layer.cornerRadius = 15
+        self.contentView.layer.borderWidth = 2
+        self.contentView.layer.borderColor = UIColor.black.cgColor
+        self.contentView.clipsToBounds = true
+        
+        // Configuração do padding
+        self.contentView.layoutMargins = UIEdgeInsets(top: 16, left: 16, bottom: 16, right: 16)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        // Ajusta o frame da célula para ocupar toda a largura da tela
+        self.contentView.frame = self.bounds.insetBy(dx: 5, dy: 5) // Adiciona espaço entre as células
+    }
 }
